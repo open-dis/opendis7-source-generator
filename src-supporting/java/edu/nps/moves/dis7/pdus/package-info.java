ae9af0f7f24207850c7e684ef6202ac168112c59
brutzman <brutzman@nps.edu>
2021-07-11 15:16:54 -0700

    only keep autogenerated package-info files for autogenerated packages

--- src-supporting/java/edu/nps/moves/dis7/pdus/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Additional infrastructure classes supporting edu.nps.moves.dis7.source.generator library.
- *
- * @see java.lang.Package
- * @see <a href="https://stackoverflow.com/questions/22095487/why-is-package-info-java-useful">https://stackoverflow.com/questions/22095487/why-is-package-info-java-useful</a>
- * @see <a href="https://stackoverflow.com/questions/624422/how-do-i-document-packages-in-java">https://stackoverflow.com/questions/624422/how-do-i-document-packages-in-java</a>
- */
-
-package edu.nps.moves.dis7.pdus;
