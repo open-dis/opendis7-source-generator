a80bfdaa3e1f1cc941f5a4f989ed70c8eb874e95
brutzman <brutzman@nps.edu>
2022-01-31 10:59:03 -0800

    javadoc

--- src-autogenerate/edu/nps/moves/dis7/source/generator/package-info.java
+++ src-autogenerate/edu/nps/moves/dis7/source/generator/package-info.java
@@ -1,5 +1,6 @@
 /**
- * Generator classes supporting edu.nps.moves.dis7.source.generator library.
+ * Generator classes supporting software production of edu.nps.moves.dis7 libraries for Java and Python.
+ * Additional programming-language support is planned as future work.
  */
 // https://stackoverflow.com/questions/624422/how-do-i-document-packages-in-java
 
