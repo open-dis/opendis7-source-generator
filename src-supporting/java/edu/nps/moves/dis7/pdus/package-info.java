a028e9e41653da492a9ad1f9f198789d2a148e11
brutzman <brutzman@nps.edu>
2025-01-22 23:14:15 -0800

    package info indicating manual authoring

---
+++ src-supporting/java/edu/nps/moves/dis7/pdus/package-info.java
@@ -0,0 +1,10 @@
+/**
+ * Utility classes supporting edu.nps.moves.dis7 library.
+ * This source code is manually authored in order to support autogeneration of new code libraries.
+ * 
+ * @see java.lang.Package
+ * @see <a href="https://stackoverflow.com/questions/22095487/why-is-package-info-java-useful">https://stackoverflow.com/questions/22095487/why-is-package-info-java-useful</a>
+ * @see <a href="https://stackoverflow.com/questions/624422/how-do-i-document-packages-in-java">https://stackoverflow.com/questions/624422/how-do-i-document-packages-in-java</a>
+ */
+
+package edu.nps.moves.dis7.pdus;
